0a51217ac9f0921360f9aa4850fc232037d6d20f
Omkar Dhanawade <dhanawadeomkar9@gmail.com>
2026-06-04 20:01:20 +0530

    Fix indentation in dashboard.py for app_mode check

--- dashboard.py
+++ dashboard.py
@@ -184,9 +184,11 @@ if run_detection:
         cap.release()
 
 else:
-    if app_mode == "Dashboard (Admin)"
+    if app_mode == "Dashboard (Admin)":
+        
         update_sidebar_active()
     else:
+        
         st.info("System Paused.")
 
 
